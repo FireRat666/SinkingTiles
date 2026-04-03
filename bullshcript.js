2e1a32e37a2bd01c2ae6898e328d0cdfd4392c35
FireRat <27763388+FireRat666@users.noreply.github.com>
2026-04-03 22:46:56 +1100

    Update

--- bullshcript.js
+++ bullshcript.js
@@ -246,7 +246,7 @@
                     }).Async();
                     await tile.AddComponent(new BS.BanterBox({ width: TILE_SIZE - 0.1, height: 0.4, depth: TILE_SIZE - 0.1 }));
                     await tile.AddComponent(new BS.BoxCollider({ size: new BS.Vector3(TILE_SIZE - 0.1, 0.4, TILE_SIZE - 0.1) }));
-                    const mat = await tile.AddComponent(new BS.BanterMaterial("Unlit/Diffuse", "", new BS.Vector4(0.2, 0.6, 1, 1), BS.MaterialSide.Front, false, tileName));
+                    const mat = await tile.AddComponent(new BS.BanterMaterial("Standard", "", new BS.Vector4(0.2, 0.6, 1, 1), BS.MaterialSide.Front, false, tileName));
 
                     // Add a separate trigger object centered so it covers the tile surface and 2m up
                     const triggerObj = await new BS.GameObject({ name: tileName + "_Trigger", parent: tile, localPosition: new BS.Vector3(0, 1.0, 0) }).Async();
